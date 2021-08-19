618cca0eab84385487f43155903eed0cdbee3279
rmnprkrl <rmnprkrl@gmail.com>
2021-08-19 18:42:40 +0200

    Add header logo

--- src/components/site/Header.js
+++ src/components/site/Header.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import { Link } from 'gatsby';
+import React, { useEffect, useState } from 'react';
 
-import NavExtra from './NavExtra';
-import NavMain from './NavMain';
-import SelectLanguage from './SelectLanguage';
+// import NavMain from './NavMain';
+import useScrollListener from '../../hooks/use-scroll-listener';
+import Logo from './Logo';
 
 const Header = () => {
+  const scroll = useScrollListener();
+  const [menuHeight, setMenuHeight] = useState('h-24');
+
+  useEffect(() => {
+    if (scroll.y > 15) {
+      setMenuHeight('h-16');
+    } else if (scroll.y < 1) {
+      setMenuHeight('h-24');
+    }
+  }, [scroll.y]);
+
   return (
-    <header>
-      <NavMain />
-      <NavExtra />
-      <nav>
-        <SelectLanguage />
-      </nav>
+    <header className="sticky top-0 z-10 border-b bg-white dark:bg-black border-gray-200 dark:border-gray-700">
+      <div className="px-4 xl:px-12">
+        <div className={`flex items-center justify-between transition-height ease-in-out ${menuHeight}`}>
+          <div className="w-40 relative">
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 
 import Layout from '../components/site/Layout';
+import NavMain from '../components/site/NavMain';
 import SEO from '../components/site/SEO';
 
 export default function IndexPage() {
@@ -11,6 +12,7 @@ export default function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <h1 className="text-9xl">{t('Hello')}</h1>
+      <NavMain />
     </Layout>
   );
 }
