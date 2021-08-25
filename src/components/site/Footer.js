66e0e6414e6660dfcf02e50593644b081829a309
rmnprkrl <rmnprkrl@gmail.com>
2021-08-25 17:12:12 +0200

    Add legal nav

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -6,13 +6,16 @@ import NavLegal from './NavLegal';
 
 const Footer = () => {
   return (
-    <footer className="w-full px-6 xl:px-12 2xl:px-28 bg-substrateDark text-white">
+    <footer className="w-full px-6 xl:px-12 2xl:px-28 bg-substrateDark text-white font-body">
       <div className="lg:flex lg:flex-row-reverse lg:justify-between lg:pt-12 lg:pb-3 border-b-2 border-gray-600">
         <div className="w-44 lg::h-7 relative py-12 lg:p-0 transform transition-all duration-300 ease-in-out hover:opacity-50">
           <Link to="/">
             <Icon name="logo-white"></Icon>
           </Link>
         </div>
+        <nav>
+          <div className="h-[600px] lg:w-full lg:max-w-screen md:h-auto flex flex-col flex-wrap lg:flex-row">nav</div>
+        </nav>
       </div>
       <div className="py-12">
         <NavLegal />
--- src/components/site/NavLegal.js
+++ src/components/site/NavLegal.js
@@ -2,25 +2,54 @@ import { useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 
 import { useSiteMenus } from '../../hooks/use-site-menus';
+import { useSiteMetadata } from '../../hooks/use-site-metadata';
+import Icon from '../default/Icon';
 import { Link } from '../default/Link';
 
 const HeaderNav = () => {
   const { t } = useTranslation();
   const { menus } = useSiteMenus();
+  const { siteMetadata } = useSiteMetadata();
+
   return (
-    <nav>
-      <ul className="list-none">
-        {menus.legal.map(menuItem => {
-          return (
-            <li key={menuItem.id}>
-              <span>
-                <Link to={menuItem.url}>{t(menuItem.id)}</Link>
-              </span>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <>
+      <div className="h-auto pb-6 md:flex md:flex-row-reverse md:justify-between md:items-center">
+        <div className="flex items-center py-4">
+          <Link
+            className="mr-11 md:mr-0 md:ml-9 transform transition-all duration-300 ease-in-out hover:opacity-50"
+            to={siteMetadata.twitter}
+          >
+            <Icon name="twitter" />
+          </Link>
+          <Link
+            className="mr-11 md:mr-0 md:ml-9 transform transition-all duration-300 ease-in-out hover:opacity-50"
+            to={siteMetadata.stackOverflow}
+          >
+            <Icon name="stackoverflow" />
+          </Link>
+          <Link
+            className="mr-11 md:mr-0 md:ml-9 transform transition-all duration-300 ease-in-out hover:opacity-50"
+            to={siteMetadata.element}
+          >
+            <Icon name="element" />
+          </Link>
+        </div>
+        <div className="underline-anchor inline-flex flex-col md:flex-row py-6 text-[#DFE3EA] text-xs font-light">
+          <div className="mb-4 md:mb-0 md:pr-8 inline-block">
+            © {new Date().getFullYear()} Parity Technologies{` `}All Rights Reserved{` `}
+          </div>
+          <ul className="list-none">
+            {menus.legal.map(menuItem => {
+              return (
+                <li key={menuItem.id} className="mb-4 md:mb-0 md:mr-8 inline-block">
+                  <Link to={menuItem.url}>{t(menuItem.id)}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
