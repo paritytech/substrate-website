796acd5a5be5e843779fb43baaa20f0ed50b6c83
joneirikboska <joneirikboska@gmail.com>
2021-08-30 13:45:54 +0200

    Create Jobs page.

--- src/pages/ecosystem/opportunities/jobs.js
+++ src/pages/ecosystem/opportunities/jobs.js
@@ -1,14 +1,32 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 
+import Icon from '../../../components/default/Icon';
+import Section from '../../../components/layout/Section';
 import Layout from '../../../components/site/Layout';
 import SEO from '../../../components/site/SEO';
+import SecondaryButton from '../../../components/ui/SecondaryButton';
 
 export default function Jobs() {
   return (
     <Layout mode="full">
       <SEO title="Jobs" />
-      <div>jobs</div>
+      <Section>
+        <div className="grid md:grid-cols-2 gap-6">
+          <div>
+            <h2 className="text-4xl font-bold my-4">Jobs</h2>
+            <p className="max-w-sm">
+              Find job opportunities in the Substrate Ecosystem through DotJobs, an independent staking provider that
+              operates validation software for protocols they are actively invested in and involved with.
+            </p>
+            <SecondaryButton link="https://dotjobs.net/">
+              <p className="mb-0 mr-2 inline">Visit DotJobs</p>
+              <Icon name="external-link" className="inline mb-1" />
+            </SecondaryButton>
+          </div>
+          <Icon name="placeholder" className="w-full h-auto rounded-md" />
+        </div>
+      </Section>
     </Layout>
   );
 }
