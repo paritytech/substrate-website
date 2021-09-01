b92cf4fab559f072b6a7990fed87618d923bf452
joneirikboska <joneirikboska@gmail.com>
2021-09-01 13:39:21 +0200

    Make Newsletter image full width.

--- src/pages/ecosystem/connect/newsletter.js
+++ src/pages/ecosystem/connect/newsletter.js
@@ -3,7 +3,6 @@ import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 import { Link } from '../../../components/default/Link';
-import Section from '../../../components/layout/Section';
 import Layout from '../../../components/site/Layout';
 import SEO from '../../../components/site/SEO';
 import PrimaryButton from '../../../components/ui/PrimaryButton';
@@ -12,16 +11,19 @@ export default function Newsletter() {
   return (
     <Layout layout="sidebar">
       <SEO title="Newsletter" />
-      <Section>
-        <div className="relative">
+      <div className="-mt-10 h-screen">
+        <div className="h-2/5 relative md:overflow-hidden">
           <StaticImage
             src="../../../images/photos/ecosystem/newsletter/newsletter.jpg"
             alt="Man on computer"
-            className="w-full h-auto"
+            className="w-full h-auto xl:-mt-16"
           />
-          <div className="bg-white lg:absolute lg:p-8 mt-8 lg:mt-0 rounded-md lg:shadow-xl max-w-md top-1/2 left-20 lg:-translate-y-1/2">
+          <div className="bg-white md:absolute m-8 md:p-8 mt-8 md:mt-0 rounded-md md:shadow-xl max-w-md top-1/2 left-10 md:-translate-y-1/2">
             <h2 className="text-4xl font-bold mb-4">Newsletter</h2>
-            <p className="font-medium text-lg">Subscribe for technical updates and helpful developer resources.</p>
+            <p className="font-medium text-lg">
+              Subscribe for technical updates
+              <br /> and helpful developer resources.
+            </p>
             <input
               placeholder="Type your Email Address"
               className="border-black border rounded-md p-3 text-center w-full mb-4"
@@ -37,7 +39,7 @@ export default function Newsletter() {
             </p>
           </div>
         </div>
-      </Section>
+      </div>
     </Layout>
   );
 }
