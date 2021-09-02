67ad9ac963554c4fb3b6bcb6e73b341d44a561b2
joneirikboska <joneirikboska@gmail.com>
2021-09-02 15:55:26 +0200

    Add strict equality checks for colorMode

--- src/components/site/ThemeToggle.js
+++ src/components/site/ThemeToggle.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { ThemeContext, ThemeProvider } from '../../contexts/ThemeContext';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import Icon from '../default/Icon';
 
 // TODO: add dark mode context
@@ -9,7 +9,7 @@ export default function ThemeToggle() {
   const { colorMode, setColorMode } = useContext(ThemeContext);
 
   function toggleTheme() {
-    if (colorMode == 'dark') {
+    if (colorMode === 'dark') {
       setColorMode('light');
     } else {
       setColorMode('dark');
@@ -17,18 +17,17 @@ export default function ThemeToggle() {
   }
 
   return (
-    <ThemeProvider>
-      <div className="cursor-pointer" onClick={toggleTheme}>
-        {colorMode == 'light' ? (
-          <div className="transform -rotate-12">
-            <Icon name="moon" />
-          </div>
-        ) : (
-          <div>
-            <Icon name="sun" />
-          </div>
-        )}
-      </div>
-    </ThemeProvider>
+    <div className="cursor-pointer" onClick={toggleTheme}>
+      {colorMode === 'light' && (
+        <div className="transform -rotate-12">
+          <Icon name="moon" />
+        </div>
+      )}
+      {colorMode === 'dark' && (
+        <div>
+          <Icon name="sun" />
+        </div>
+      )}
+    </div>
   );
 }
