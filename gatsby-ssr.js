ceb5a0e2e6eba19fec66dbefa057fdefd8374cf9
joneirikboska <joneirikboska@gmail.com>
2021-09-02 10:47:58 +0200

    Comment out all gatsby-ssr.js code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,41 +1,41 @@
-import React from 'react';
+// import React from 'react';
 
-import { ThemeProvider } from './src/contexts/ThemeContext';
+// import { ThemeProvider } from './src/contexts/ThemeContext';
 
-const MagicScriptTag = () => {
-  const codeToRunOnClient = `
-  (function() {
-    function getInitialColorMode() {
-        const persistedColorPreference = localStorage.theme;
-        const hasPersistedPreference = typeof persistedColorPreference === 'string';
-      
-        if (hasPersistedPreference) {
-          return persistedColorPreference;
-        }
-      
-        const mql = window.matchMedia('(prefers-color-scheme: dark)');
-        const hasMediaQueryPreference = typeof mql.matches === 'boolean';
-        if (hasMediaQueryPreference) {
-          return mql.matches ? 'dark' : 'light';
-        }
-      
-        return 'light';
-    }
-    const colorMode = getInitialColorMode();
+// const MagicScriptTag = () => {
+//   const codeToRunOnClient = `
+//   (function() {
+//     function getInitialColorMode() {
+//         const persistedColorPreference = localStorage.theme;
+//         const hasPersistedPreference = typeof persistedColorPreference === 'string';
 
-    document.documentElement.classList.add(colorMode);
-  })()
-    `;
-  // eslint-disable-next-line react/no-danger
-  return <script dangerouslySetInnerHTML={{ __html: codeToRunOnClient }} />;
-};
+//         if (hasPersistedPreference) {
+//           return persistedColorPreference;
+//         }
 
-export const onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents(<MagicScriptTag />);
-};
+//         const mql = window.matchMedia('(prefers-color-scheme: dark)');
+//         const hasMediaQueryPreference = typeof mql.matches === 'boolean';
+//         if (hasMediaQueryPreference) {
+//           return mql.matches ? 'dark' : 'light';
+//         }
 
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>
-    <div>{element}</div>
-  </ThemeProvider>
-);
+//         return 'light';
+//     }
+//     const colorMode = getInitialColorMode();
+
+//     document.documentElement.classList.add(colorMode);
+//   })()
+//     `;
+//   // eslint-disable-next-line react/no-danger
+//   return <script dangerouslySetInnerHTML={{ __html: codeToRunOnClient }} />;
+// };
+
+// export const onRenderBody = ({ setPreBodyComponents }) => {
+//   setPreBodyComponents(<MagicScriptTag />);
+// };
+
+// export const wrapRootElement = ({ element }) => (
+//   <ThemeProvider>
+//     <div>{element}</div>
+//   </ThemeProvider>
+// );
