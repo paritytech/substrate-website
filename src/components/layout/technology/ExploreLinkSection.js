3422072af1539da2528170a9d88f762ad9e30ecb
joneirikboska <joneirikboska@gmail.com>
2021-09-06 16:06:44 +0200

    Keep menu arrow down green if selected and not hovered

--- src/components/layout/technology/ExploreLinkSection.js
+++ src/components/layout/technology/ExploreLinkSection.js
@@ -40,7 +40,9 @@ const ExploreLinkSection = () => {
               <h5 className="text-xl font-bold mb-6">{title}</h5>
               <p>{description}</p>
               <Link to={link}>
-                <p className="font-bold border-b-2 border-black inline hover:mr-1 transition-all">Explore</p>{' '}
+                <p className="font-bold border-b-2 border-black dark:border-white inline hover:mr-1 transition-all">
+                  Explore
+                </p>{' '}
                 <span className="fill-current border-b-0 dark:text-white inline-block">
                   <Icon name="arrow-more" />
                 </span>
