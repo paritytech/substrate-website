259c70ad99e1d8e5698fef1803e45f5e0045bedc
joneirikboska <joneirikboska@gmail.com>
2021-09-06 15:15:10 +0200

    Set mobile menu dark mode colors

--- src/components/site/NavMobile.js
+++ src/components/site/NavMobile.js
@@ -11,7 +11,7 @@ const NavMobile = ({ toggleMenu }) => {
   const { t } = useTranslation();
 
   return (
-    <div className="lg:hidden absolute inset-0 bg-substrateGray-light dark:bg-black z-90 animate-fade-in-right">
+    <div className="lg:hidden absolute inset-0 bg-substrateGray-light dark:bg-substrateDarkest z-90 animate-fade-in-right">
       <div className="h-16 px-6 flex items-center justify-between">
         <div className="w-32">
           <Link to="/">
@@ -22,13 +22,13 @@ const NavMobile = ({ toggleMenu }) => {
           <Icon name="close-x" />
         </div>
       </div>
-      <div className="bg-substrateGray-light dark:bg-black h-screen z-20">
+      <div className="bg-substrateGray-light dark:bg-substrateDarkest h-screen z-20">
         <div className="py-8">
           {menus.main.map(menuItem => {
             return (
               <div
                 key={menuItem.id}
-                className="py-3 hover:bg-substrateGreen-light dark:hover:bg-gray-700 font-medium transform transition-all duration-75 ease-in-out hover:font-bold"
+                className="py-3 hover:bg-substrateGreen-light dark:hover:bg-green-700 font-medium transform transition-all duration-75 ease-in-out hover:font-bold"
               >
                 <div className="px-6 flex items-center justify-between focus:outline-none cursor-pointer">
                   <div className="text-2xl">{t(menuItem.id)}</div>
