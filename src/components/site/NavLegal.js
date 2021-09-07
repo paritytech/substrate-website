509ec56c42c515157ecda95977f10a0250d8e1a9
rmnprkrl <rmnprkrl@gmail.com>
2021-09-07 18:35:19 +0200

    fix so link

--- src/components/site/NavLegal.js
+++ src/components/site/NavLegal.js
@@ -24,7 +24,7 @@ const HeaderNav = () => {
           className="mr-11 md:mr-0 md:ml-9 transform transition-all duration-300 ease-in-out hover:opacity-50"
           to={siteMetadata.stackoverflow}
         >
-          <Icon name="stackoverflow" />
+          <Icon name="stack-overflow" />
         </Link>
         <Link
           className="mr-11 md:mr-0 md:ml-9 transform transition-all duration-300 ease-in-out hover:opacity-50"
