48258a2a1fe657c202c5ff43828d50a8cf8cc064
joneirikboska <joneirikboska@gmail.com>
2021-09-22 13:11:38 +0200

    Place background animation in CardsNav

--- src/components/layout/technology/CardsNav.js
+++ src/components/layout/technology/CardsNav.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Lottie from 'react-lottie';
 
+import * as animationData from '../../../images/animation/technology/overview.json';
 import Section from '../Section';
 import Card from './Card';
 
@@ -9,7 +11,11 @@ const CardsNav = ({ currentPage }) => {
   return (
     <Section>
       <div className="relative">
-        <div className="hidden lg:block absolute h-80 w-1/2 bg-gray-200 -right-4 z-0"></div>
+        <div className="hidden lg:block absolute w-1/2 -right-4 z-0">
+          <div className="w-full aspect-w-10 aspect-h-7 mb-8">
+            <Lottie options={{ animationData }} height="full" width="full" />
+          </div>
+        </div>
         <h2 className="text-3xl font-bold md:text-4xl mb-10 py-10 relative">Learn More About Our Technology</h2>
         <div className="grid grid-cols-1 gap-10 lg:gap-0 lg:flex flex-col lg:flex-row z-10">
           {pages
