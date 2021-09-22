d67d70eeb13159d011f1173937b4a14a2fb40ba7
joneirikboska <joneirikboska@gmail.com>
2021-09-22 13:06:14 +0200

    Adjust HighlightBanner layout

--- src/components/layout/technology/HighlightBanner.js
+++ src/components/layout/technology/HighlightBanner.js
@@ -5,9 +5,9 @@ import Icon from '../../default/Icon';
 const HighlightBanner = ({ icon, children }) => {
   return (
     <div className="bg-substrateGray-light dark:bg-substrateBlackish p-8 mb-20">
-      <div className="flex flex-col md:flex-row md:items-center lg:mx-auto max-w-7xl">
-        <Icon name={icon} className="md:mx-auto pr-16 md:pr-0 w-1/2 md:max-h-52" />
-        <div className="md:mx-8">
+      <div className="md:grid md:grid-cols-3 md:items-center lg:mx-auto max-w-7xl">
+        <Icon name={icon} className="pr-16 md:pr-0 w-1/2 md:max-h-52 md:justify-self-end" />
+        <div className="md:mx-8 col-span-2">
           <p className="m-0 p-0 text-xl md:text-2xl font-normal max-w-2xl">{children}</p>
         </div>
       </div>
