c92973c7170d55cb03d00d6df3c9a467c62a21d6
joneirikboska <joneirikboska@gmail.com>
2021-09-29 14:59:23 +0200

    Place heading and paragraph, move modal close button

--- src/pages/ecosystem/projects.js
+++ src/pages/ecosystem/projects.js
@@ -49,29 +49,40 @@ export default function Projects({ data }) {
         />
       )}
       <SEO title="Teams" />
+
       <Section>
-        <div
-          onClick={() => modalSetOpen(true)}
-          className="mt-32 mb-44 aspect-w-4 aspect-h-3 md:aspect-w-16 md:aspect-h-9 lg:aspect-w-12 lg:aspect-h-5 bg-substrateDark rounded-md shadow-xl hover:scale-105 transition-transform cursor-pointer"
-        >
-          <div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="self-center lg:pr-12">
+            <h1 className="text-4xl lg:text-5xl font-bold xl:text-6xl">Meet The Teams</h1>
+            <p className="font-semibold text-xl lg:text-2xl">More than 150 projects are building on Substrate</p>
+            <p>
+              Some of the most exciting projects in decentralized tech are building on Substrate, creating Polkadot
+              parachains, independent layer-1 blockchains, and infrastructure.
+            </p>
+            <p>Start building</p>
+          </div>
+          <div
+            onClick={() => modalSetOpen(true)}
+            className="flex-none w-full lg:w-1/2 relative rounded-md shadow-xl hover:scale-105 transition-transform cursor-pointer"
+          >
             <StaticImage
               src="../images/photos/ecosystem/home/youtube.jpg"
-              className="w-full h-full m-0 relative rounded-md"
+              className="aspect-w-3 aspect-h-2 w-full h-full m-0 relative rounded-md"
             />
+            <h3 className="absolute left-0 top-0 p-8 text-white font-bold text-2xl">Meet Substrate teams</h3>
             <div className="absolute left-0 bottom-0 p-8">
-              <h3 className="text-white mb-2">Meet Substrate teams</h3>
               <p className="text-white opacity-75 m-0 text-lg">
                 Find out why more than 150 projects are building on Substrate.
               </p>
             </div>
             <Icon
               name="play"
-              className="hidden sm:block absolute left-2/4 top-2/4 transform -translate-x-2/4 -translate-y-2/4 hover:scale-105 transition-transform"
+              className="w-12 h-12 absolute left-2/4 top-2/4 transform -translate-x-2/4 -translate-y-2/4 hover:scale-105 transition-transform"
             />
           </div>
         </div>
       </Section>
+
       <section className="container px-6 mb-20">
         <h2 id="projects" className="mb-8 scroll-margin-top-100">
           Projects
@@ -87,6 +98,7 @@ export default function Projects({ data }) {
           logos={logos}
         />
       </section>
+
       {isComponentVisible && (
         <>
           <div
