736f667eb18f51a972467c40991d491c6be9f681
rmnprkrl <rmnprkrl@gmail.com>
2021-09-29 15:42:56 +0200

    remove quotes fx

--- src/components/ui/Blockquote.js
+++ src/components/ui/Blockquote.js
@@ -2,14 +2,8 @@ import React from 'react';
 
 const Blockquote = ({ children }) => {
   return (
-    <aside
-      data-aos="fade"
-      data-aos-delay="600"
-      className="border-substrateGreen border-l-8 pl-4 my-8 overflow-x-hidden"
-    >
-      <div className="font-bold" data-aos="fade-left" data-aos-delay="700" data-aos-duration="350">
-        {children}
-      </div>
+    <aside className="border-substrateGreen border-l-8 pl-4 my-8 overflow-x-hidden">
+      <div className="font-bold">{children}</div>
     </aside>
   );
 };
