7d57df7369b8ddea55f4aa2b9b9b866b61c2ac93
rmnprkrl <rmnprkrl@gmail.com>
2021-09-30 10:41:45 +0200

    improve docs button load

--- src/components/site/DocsButton.js
+++ src/components/site/DocsButton.js
@@ -7,8 +7,11 @@ export default function DocsButton() {
   return (
     // TODO: add docs link
     // TODO: add docs title i18n
-    <Link to="https://substrate-docs-staging.netlify.app/v3">
-      <button className="flex items-center justify-center bg-substrateDark dark:bg-white  text-sm py-2 w-20 rounded opacity-100 transform transition duration-300 ease-in-out hover:opacity-80 focus:outline-none fill-current text-white dark:text-black">
+    <Link
+      to="https://substrate-docs-staging.netlify.app/v3"
+      className="block opacity-100 transform transition duration-300 ease-in-out hover:opacity-80"
+    >
+      <button className="flex items-center justify-center bg-substrateDark dark:bg-white  text-sm py-2 w-20 rounded focus:outline-none fill-current text-white dark:text-black">
         <Icon name="docs" />
         <span className="pl-2">Docs</span>
       </button>
