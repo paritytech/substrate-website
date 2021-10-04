43407535d77adb3d6570e5a4967bcedb79005f8b
joneirikboska <joneirikboska@gmail.com>
2021-10-04 10:55:35 +0200

    Specify sun icon text black

--- src/components/site/ThemeToggle.js
+++ src/components/site/ThemeToggle.js
@@ -16,7 +16,7 @@ export default function ThemeToggle() {
 
   return (
     <div className="cursor-pointer hover:opacity-60 transition-opacity" onClick={toggleTheme}>
-      {colorMode === 'light' && <Icon name="sun" className="opacity-50" />}
+      {colorMode === 'light' && <Icon name="sun" className="opacity-50 fill-current text-black" />}
       {colorMode === 'dark' && <Icon name="moon" className="opacity-50 fill-current text-white transform -rotate-12" />}
     </div>
   );
