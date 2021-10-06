a9f85cbb6fb321e5015282ea805c8661a8ff1a7d
joneirikboska <joneirikboska@gmail.com>
2021-10-06 12:32:55 +0200

    Prevent IconCard title line break

--- src/components/layout/ecosystem/IconCard.js
+++ src/components/layout/ecosystem/IconCard.js
@@ -13,10 +13,10 @@ const IconCard = ({ title, description, link, delay, icon, color, linkText }) =>
   return (
     <Link to={link} data-aos="fade-up" data-aos-delay={delay ? delay : 0}>
       <div className="cursor-pointer shadow-xl px-6 py-8 hover:scale-105 transition-transform rounded-md bg-white dark:bg-substrateBlackish relative min-h-full">
-        <h3 className="h5">
+        <div className="flex">
           {titleIcon}
-          {title}
-        </h3>
+          <h3 className="font-bold text-xl mb-4">{title}</h3>
+        </div>
         <div className="flex flex-col justify-between">
           <div className="mb-16 mt-6 pl-5 relative">
             <div
