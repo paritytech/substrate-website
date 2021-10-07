5e65c7521b784bd6e9a30c89c6e96d14b9d140bc
joneirikboska <joneirikboska@gmail.com>
2021-10-07 14:42:56 +0200

    Change placeholder to none

--- src/pages/ecosystem/connect/newsletter.js
+++ src/pages/ecosystem/connect/newsletter.js
@@ -15,7 +15,7 @@ export default function NewsletterPage() {
           src="../../../images/photos/ecosystem/newsletter/newsletter.jpg"
           alt="Man on computer"
           className="w-full h-auto"
-          placeholder="blurred"
+          placeholder="none"
         />
         <div className="bg-white dark:bg-transparent md:dark:bg-substrateBlackish p-4 my-6 md:my-0 md:absolute md:p-8 rounded-md md:shadow-xl max-w-lg top-1/2 left-10 md:-translate-y-1/2">
           <Newsletter />
