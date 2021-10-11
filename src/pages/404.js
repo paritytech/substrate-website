b9fe929dcbc4f1cb0a8f6417158619c9422f6811
Imad Arain <arain.imad@gmail.com>
2021-10-11 08:48:57 -0400

    updated button text and link to home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import { graphql, navigate } from 'gatsby';
+import { graphql } from 'gatsby';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 
@@ -16,7 +16,7 @@ export default function NotFoundPage() {
         <div className="w-2/3 mx-auto mt-20 text-center self-center">
           <h1 className="mb-10 four-oh-four-title text-9xl xl:text-four-oh-four font-body">{t('404.title')}</h1>
           <h2 className="text-4xl mb-10 font-bold">{t('404.message')}</h2>
-          <PrimaryButtonLink onClick={() => navigate(-1)}>Go back</PrimaryButtonLink>
+          <PrimaryButtonLink link="/">Home</PrimaryButtonLink>
         </div>
       </Section>
     </Layout>
