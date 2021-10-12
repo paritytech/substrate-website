ca67e8cb553ac6f1426e66ab1c6b3fda7f94a01a
rmnprkrl <rmnprkrl@gmail.com>
2021-10-12 21:19:16 +0200

    bypass scroll to top when article nav

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,7 +11,29 @@ export const wrapRootElement = ({ element }) => <ThemeProvider>{element}</ThemeP
 export const wrapPageElement = ({ element, props }) => <DataProvider props={props}>{element}</DataProvider>;
 
 // reset scroll position to top
-export const shouldUpdateScroll = () => {
+// export const shouldUpdateScroll = () => {
+//   window.scrollTo(0, 0);
+//   return false;
+// };
+
+/* overide update scroll position on route change;
+   see: https://github.com/alexluong/gatsby-packages/issues/33 */
+export const shouldUpdateScroll = ({ routerProps: { location } }) => {
+  if (!window || !location) {
+    return [0, 0];
+  }
+  // TODO: bypass forcing scroll programmatically on pages with article nav
+  const keepScroll = [
+    '/vision/substrate-and-polkadot',
+    '/ecosystem/resources/awesome-substrate',
+    '/ecosystem/projects/case-studies/acala',
+    '/ecosystem/projects/case-studies/moonbeam',
+    '/ecosystem/projects/case-studies/astar',
+  ];
+  if (keepScroll.includes(location.pathname)) {
+    return false;
+  }
+
   window.scrollTo(0, 0);
   return false;
 };
