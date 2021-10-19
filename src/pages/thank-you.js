90e8eda17042d2ee0eb9b01d1196124ffb9c1096
joneirikboska <joneirikboska@gmail.com>
2021-10-19 12:20:18 +0200

    Remove illustration placeholder

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,7 +1,6 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 
-import Icon from '../components/default/Icon';
 import { Link } from '../components/default/Link';
 import Section from '../components/layout/Section';
 import Layout from '../components/site/Layout';
@@ -11,15 +10,13 @@ export default function ThankYou() {
   return (
     <Layout>
       <SEO title="Thank You" />
-      <Section col={2} children2={<Icon name="placeholder" className="w-full h-auto" />}>
-        <div className="h-full flex">
-          <div className="self-center pr-8">
-            <h1 className="mb-20 text-5xl md:text-6xl font-body font-extrabold">Thanks For Your Submission.</h1>
-            <p className="underline-animate underline-animate-thin">
-              For more news, follow <Link to="https://twitter.com/substrate_io">@substrate_io</Link> on Twitter.
-            </p>
-          </div>
-        </div>
+      <Section>
+        <h1 className="mb-20 mt-20 md:mt-52 text-4xl sm:text-5xl md:text-6xl font-body font-extrabold">
+          Thanks For Your Submission.
+        </h1>
+        <p className="underline-animate underline-animate-thin">
+          For more news, follow <Link to="https://twitter.com/substrate_io">@substrate_io</Link> on Twitter.
+        </p>
       </Section>
     </Layout>
   );
