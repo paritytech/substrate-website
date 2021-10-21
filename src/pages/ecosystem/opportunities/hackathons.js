94816f76e2c101af6688b42154362de440fdf91e
joneirikboska <joneirikboska@gmail.com>
2021-10-21 11:25:32 +0200

    Map hackathons only once, and conditionally render heading

--- src/pages/ecosystem/opportunities/hackathons.js
+++ src/pages/ecosystem/opportunities/hackathons.js
@@ -10,16 +10,11 @@ import { useHackathons } from '../../../hooks/use-hackathons';
 export default function Hackathons() {
   const { hackathons } = useHackathons();
 
-  const upcoming = [];
-  const past = [];
-
-  hackathons.forEach(hackathon => {
-    if (new Date(hackathon.node.frontmatter.date_end) >= new Date()) {
-      upcoming.push(hackathon);
-    } else {
-      past.push(hackathon);
+  const isUpcoming = event => {
+    if (new Date(event.node.frontmatter.date_end) >= new Date()) {
+      return true;
     }
-  });
+  };
 
   return (
     <Layout layout="sidebar">
@@ -29,22 +24,21 @@ export default function Hackathons() {
         <p>
           Challenge yourself, up your game, and connect with the commmunity. There are great prizes up for grabs, too.
         </p>
-        {upcoming.length > 0 && (
-          <div className="mb-16">
-            <h3 className="font-medium text-2xl mb-8 mt-10">Upcoming events:</h3>
-            {upcoming.map((hackathon, idx) => (
-              <HackathonCard key={idx} hackathon={hackathon} />
-            ))}
-          </div>
-        )}
-        {past.length > 0 && (
-          <>
-            <h3 className="font-medium text-2xl mb-8 mt-10">Past events:</h3>
-            {past.map((hackathon, idx) => (
-              <HackathonCard key={idx} hackathon={hackathon} />
-            ))}
-          </>
-        )}
+        {hackathons.map((hackathon, idx) => {
+          return (
+            <div key={idx}>
+              {isUpcoming(hackathon) && idx === 0 ? (
+                <h3 className="font-medium text-2xl mb-8 mt-14">Upcoming events:</h3>
+              ) : (
+                idx === 0 ||
+                (!isUpcoming(hackathon) && isUpcoming(hackathons[idx - 1]) && (
+                  <h3 className="font-medium text-2xl mb-8 mt-14">Past events:</h3>
+                ))
+              )}
+              <HackathonCard hackathon={hackathon} />
+            </div>
+          );
+        })}
       </Section>
     </Layout>
   );
