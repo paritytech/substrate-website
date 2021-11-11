7195042ea3a17425634c91008c0e5f0cc4ce08e6
joneirikboska <joneirikboska@gmail.com>
2021-11-11 13:56:44 +0100

    Reduce banner height

--- src/components/site/Banner.js
+++ src/components/site/Banner.js
@@ -5,7 +5,7 @@ import { Link } from '../default/Link';
 const Banner = () => {
   return (
     <div className="bg-substrateGray-light dark:bg-substrateBlackish m-0">
-      <p className="text-center py-4 mb-0 underline-animate underline-animate-thin">
+      <p className="text-center py-3 mb-0 underline-animate underline-animate-thin">
         Substrate is campaigning to get its own StackExchange Community created. Please help us{' '}
         <Link to="https://link.com">here.</Link>
       </p>
