4a6a80d52ca5fd205d995850c2f673a354ae2f97
joneirikboska <joneirikboska@gmail.com>
2021-11-11 14:49:10 +0100

    Hide banner on scroll

--- src/components/site/Banner.js
+++ src/components/site/Banner.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import cx from 'classnames';
+import React, { useEffect, useState } from 'react';
 
+import useScrollListener from '../../hooks/use-scroll-listener';
 import { Link } from '../default/Link';
 
 const Banner = () => {
+  const scroll = useScrollListener();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    if (scroll.y > 30) {
+      setIsScrolled(true);
+    } else if (scroll.y < 1) {
+      setIsScrolled(false);
+    }
+  }, [scroll.y]);
+
   return (
-    <div className="bg-substrateGray-light dark:bg-substrateBlackish m-0">
-      <p className="text-center py-3 mb-0 underline-animate underline-animate-thin">
+    <div
+      className={cx('bg-substrateGray-light transition-all dark:bg-substrateBlackish m-0', {
+        '-mt-24': isScrolled,
+      })}
+    >
+      <p className="text-center py-3 px-4 mb-0 underline-animate underline-animate-thin">
         Substrate is campaigning to get its own StackExchange Community created. Please help us{' '}
         <Link to="https://link.com">here.</Link>
       </p>
