4446d37e2a60c57f2a61814f3f272f971cddd8ca
joneirikboska <joneirikboska@gmail.com>
2021-11-11 13:54:43 +0100

    Create banner at top of all pages

---
+++ src/components/site/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Link } from '../default/Link';
+
+const Banner = () => {
+  return (
+    <div className="bg-substrateGray-light dark:bg-substrateBlackish m-0">
+      <p className="text-center py-4 mb-0 underline-animate underline-animate-thin">
+        Substrate is campaigning to get its own StackExchange Community created. Please help us{' '}
+        <Link to="https://link.com">here.</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
