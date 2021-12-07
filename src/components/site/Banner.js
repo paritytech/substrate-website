a43a87e3773715e93648ddcdc559751214f5fabb
je-boska <joneirikboska@gmail.com>
2021-12-07 15:43:10 +0100

    Refactor conditional rendering

--- src/components/site/Banner.js
+++ src/components/site/Banner.js
@@ -16,26 +16,29 @@ const Banner = () => {
     },
   } = banner;
 
-  if (!isBannerOpen) {
-    return null;
-  }
-
   return (
-    <div
-      className={cx(
-        'hidden md:block fixed z-50 right-10 bottom-8 max-w-sm p-6 transition-all text-white dark:text-black dark:bg-substrateGray bg-substrateBlackish m-0 shadow-xxl rounded-md'
-      )}
-    >
-      <h4 className="mb-2 font-bold text-2xl">{title}</h4>
-      <div dangerouslySetInnerHTML={{ __html: html }} className="banner underline-animate underline-animate-thin"></div>
+    <>
+      {isBannerOpen && (
+        <div
+          className={cx(
+            'hidden md:block fixed z-50 right-10 bottom-8 max-w-sm p-6 transition-all text-white dark:text-black dark:bg-substrateGray bg-substrateBlackish m-0 shadow-xxl rounded-md'
+          )}
+        >
+          <h4 className="mb-2 font-bold text-2xl">{title}</h4>
+          <div
+            dangerouslySetInnerHTML={{ __html: html }}
+            className="banner underline-animate underline-animate-thin"
+          ></div>
 
-      <div
-        className="absolute right-4 top-4 cursor-pointer duration-150 ease-in-out hover:scale-110"
-        onClick={() => setIsBannerOpen(false)}
-      >
-        <Icon name="close-x" className="fill-current h-4 w-4" />
-      </div>
-    </div>
+          <div
+            className="absolute right-4 top-4 cursor-pointer duration-150 ease-in-out hover:scale-110"
+            onClick={() => setIsBannerOpen(false)}
+          >
+            <Icon name="close-x" className="fill-current h-4 w-4" />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
