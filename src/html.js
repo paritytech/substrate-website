ac0d2308551c6ec8cc872baf2412946a8edbc059
Imad Arain <arain.imad@gmail.com>
2021-12-08 08:56:20 -0500

    adding transparent pixels

--- src/html.js
+++ src/html.js
@@ -1,9 +1,26 @@
-/* replace default gatsby html
-	 - https://www.gatsbyjs.com/docs/custom-html
-*/
-
 import PropTypes from 'prop-types';
 import React from 'react';
+
+import TransparentPixel from './components/site/TransparentPixel';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+        {props.postBodyComponents}
+        <TransparentPixel />
+      </body>
+    </html>
+  );
+}
 
 HTML.propTypes = {
   htmlAttributes: PropTypes.object,
@@ -13,26 +30,3 @@ HTML.propTypes = {
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
 };
-
-export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes} className="">
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes} className="Site">
-        {props.preBodyComponents}
-        <div
-          key={`body`}
-          id="___gatsby"
-          className="Site-container"
-          dangerouslySetInnerHTML={{ __html: props.body || '' }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  );
-}
