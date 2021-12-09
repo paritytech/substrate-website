2eac3a4c06621c96d0b6a514451bda38d9494a19
je-boska <joneirikboska@gmail.com>
2021-12-09 16:43:08 +0100

    Create renderBanner state, default to null

--- src/components/site/Banner.js
+++ src/components/site/Banner.js
@@ -1,18 +1,23 @@
 import cx from 'classnames';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useBanner } from '../../hooks/use-banner';
 import { useSessionStorage } from '../../hooks/use-session-storage';
 import Icon from '../default/Icon';
 
 const Banner = () => {
+  const [renderBanner, setRenderBanner] = useState(null);
   const { isBannerOpen, setIsBannerOpen } = useSessionStorage('banner', true);
 
   const banners = useBanner();
 
+  useEffect(() => {
+    setRenderBanner(isBannerOpen);
+  }, [isBannerOpen]);
+
   return (
     <>
-      {isBannerOpen && (
+      {renderBanner && (
         <div
           className={cx(
             'hidden md:block fixed z-50 right-10 bottom-8 max-w-sm p-6 transition-all text-white dark:text-black dark:bg-substrateGray bg-substrateBlackish m-0 shadow-xxl rounded-md'
@@ -28,14 +33,14 @@ const Banner = () => {
               },
               idx
             ) => (
-              <>
+              <div key={idx}>
                 {idx !== 0 && <div className="h-0.5 my-2 bg-substrateDarkThemeGrey dark:bg-substrateSubtleGrey"></div>}
                 <h4 className="mb-2 font-bold text-2xl">{title}</h4>
                 <div
                   dangerouslySetInnerHTML={{ __html: html }}
                   className="banner underline-animate underline-animate-thin"
                 ></div>
-              </>
+              </div>
             )
           )}
 
