b825993dd9a23ff4806861d21770e4cec45058f5
je-boska <joneirikboska@gmail.com>
2021-12-14 10:21:54 +0100

    Recreate checkbox as pseudo element

--- src/components/layout/ecosystem/teams/CaseStudyFilter.js
+++ src/components/layout/ecosystem/teams/CaseStudyFilter.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 const CaseStudyFilter = ({ showCaseStudies, setShowCaseStudies }) => {
   return (
-    <div className="lg:text-right mb-6 lg:-mb-6">
-      <label htmlFor="case-studies">Show only Case Studies</label>
+    <div className="flex w-full lg:justify-end lg:-mb-14 case-studies">
+      <p className="inline mr-3 cursor-default" onClick={() => setShowCaseStudies(!showCaseStudies)}>
+        Show only Case Studies
+      </p>
       <input
-        className="rounded-sm ml-2 mb-0.5"
         type="checkbox"
         id="case-studies"
         value={showCaseStudies}
         checked={showCaseStudies}
         onChange={() => setShowCaseStudies(!showCaseStudies)}
       />
+      <label htmlFor="case-studies"></label>
     </div>
   );
 };
