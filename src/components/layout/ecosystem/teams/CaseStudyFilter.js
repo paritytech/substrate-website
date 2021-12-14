2987807a9c9acf2e38d073b914777e7888dab79a
joneirikboska <joneirikboska@gmail.com>
2021-12-14 14:11:01 +0100

    Remove inactive space in CaseStudyFilter

--- src/components/layout/ecosystem/teams/CaseStudyFilter.js
+++ src/components/layout/ecosystem/teams/CaseStudyFilter.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CaseStudyFilter = ({ showCaseStudies, setShowCaseStudies }) => {
   return (
     <div className="flex w-full lg:justify-end lg:-mb-14 case-studies">
-      <p className="inline mr-3 cursor-pointer" onClick={() => setShowCaseStudies(!showCaseStudies)}>
+      <p className="inline pr-3 cursor-pointer" onClick={() => setShowCaseStudies(!showCaseStudies)}>
         Show only Case Studies
       </p>
       <input
