debae5829ec66e56ec64f1fb346c6b52f0f19c0e
je-boska <joneirikboska@gmail.com>
2021-12-22 11:55:46 +0100

    Add newsletter component to footer

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -1,13 +1,18 @@
 import { Link } from 'gatsby';
-import React from 'react';
+import React, { useContext } from 'react';
 
+import DataContext from '../../contexts/DataContext';
 import Icon from '../default/Icon';
 import NavFooter from './NavFooter';
 import NavLegal from './NavLegal';
+import Newsletter from './newsletter';
 
 const Footer = () => {
+  const { location } = useContext(DataContext);
+
   return (
     <footer className="w-full px-4 xl:px-12 bg-substrateDark text-white font-body">
+      {!location.pathname.includes('newsletter') && <Newsletter footer />}
       <div className="lg:flex lg:flex-row-reverse lg:justify-between lg:pt-12 lg:pb-3 border-b-2 border-gray-600">
         <div className="w-52 lg:h-7 relative py-12 lg:p-0 transform transition-all duration-300 ease-in-out hover:opacity-50">
           <Link to="/">
--- src/components/site/newsletter.js
+++ src/components/site/newsletter.js
@@ -1,9 +1,11 @@
+import cn from 'classnames';
 import React, { useState } from 'react';
 
 import { useSiteMetadata } from '../../hooks/use-site-metadata';
+import Icon from '../default/Icon';
 import { Link } from '../default/Link';
 
-export default function Newsletter() {
+export default function Newsletter({ footer = false }) {
   const [formSubmitted, setFormSubmitted] = useState(false);
   const { siteMetadata } = useSiteMetadata();
 
@@ -28,47 +30,54 @@ export default function Newsletter() {
   return (
     <>
       {!formSubmitted && (
-        <div className="w-full">
-          <h2 className="text-4xl font-bold mb-4">Newsletter</h2>
-          <p className="font-bold text-lg">
-            Subscribe for the latest news, technical updates and helpful developer resources.
-          </p>
-          <form onSubmit={event => handleSubmit(event)}>
-            <input type="hidden" name="u" value="11" />
-            <input type="hidden" name="f" value="11" />
-            <input type="hidden" name="s" />
-            <input type="hidden" name="c" value="0" />
-            <input type="hidden" name="m" value="0" />
-            <input type="hidden" name="act" value="sub" />
-            <input type="hidden" name="v" value="2" />
-            <div>
-              <input
-                className="w-full h-16 flex-1 mb-6 border-3 rounded-lg border-black dark:bg-substrateDarkest dark:border-substrateGray-dark dark:placeholder-white text-bodyBg text-xl p-4 focus:outline-none hover:ring-2 focus:ring-2 text-center"
-                type="email"
-                name="email"
-                placeholder="Email Address"
-                required
-              />
-              <button
-                className="w-full bg-substrateGreen hover:bg-white text-white hover:text-substrateGreen align-items mb-6 px-9 py-4 text-xl font-bold transition duration-200 focus:outline-none focus:ring-2 rounded-lg border-3 border-substrateGreen"
-                type="submit"
-                value="Submit"
-                onSubmit={event => handleSubmit(event)}
-              >
-                Subscribe
-              </button>
-            </div>
-            <p className="text-sm">
-              To see how we use your information please see our{' '}
-              <span className="underline-animate underline-animate-thin">
-                <Link to="https://www.parity.io/privacy/">privacy policy</Link>
-              </span>
+        <div className={cn('w-full', { ' pt-8 border-b-2 border-gray-600': footer })}>
+          <div className="flex items-center">
+            <Icon name="paperplane" className="mb-4 mr-4 fill-current text-white" />
+            <h2 className="text-4xl font-bold mb-4">Newsletter</h2>
+          </div>
+          <div className={cn('', { 'lg:grid lg:grid-cols-3 gap-4': footer })}>
+            <p className="font-bold text-lg max-w-lg">
+              Subscribe for the latest news, technical updates and helpful developer resources.
             </p>
-          </form>
+            <form className="col-span-2" onSubmit={event => handleSubmit(event)}>
+              <input type="hidden" name="u" value="11" />
+              <input type="hidden" name="f" value="11" />
+              <input type="hidden" name="s" />
+              <input type="hidden" name="c" value="0" />
+              <input type="hidden" name="m" value="0" />
+              <input type="hidden" name="act" value="sub" />
+              <input type="hidden" name="v" value="2" />
+              <div className={cn('', { 'lg:grid grid-cols-2 gap-4': footer })}>
+                <input
+                  className="w-full max-w-lg h-16 flex-1 mb-6 border-3 rounded-lg border-black dark:bg-substrateDarkest dark:border-substrateGray-dark dark:placeholder-white text-bodyBg text-xl p-4 focus:outline-none hover:ring-2 focus:ring-2 text-center"
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                />
+                <div>
+                  <button
+                    className="w-full max-w-lg bg-substrateGreen hover:bg-white text-white hover:text-substrateGreen align-items mb-6 px-9 py-4 text-xl font-bold transition duration-200 focus:outline-none focus:ring-2 rounded-lg border-3 border-substrateGreen"
+                    type="submit"
+                    value="Submit"
+                    onSubmit={event => handleSubmit(event)}
+                  >
+                    Subscribe
+                  </button>
+                  <p className="text-sm">
+                    To see how we use your information please see our{' '}
+                    <span className="underline-animate underline-animate-thin">
+                      <Link to="https://www.parity.io/privacy/">privacy policy</Link>
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       )}
       {formSubmitted && (
-        <div className="w-full">
+        <div className={cn('w-full', { 'pt-8': footer })}>
           <h2 className="text-4xl font-bold mb-4">Almost finished...</h2>
           <p className="font-bold text-lg">
             To complete the subscription process, please click the link in the email we just sent you.
