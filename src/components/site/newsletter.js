df512ead4db15044afe477799cec4cef37b4f2be
joneirikboska <joneirikboska@gmail.com>
2022-01-05 12:26:16 +0100

    Create Subscribe button loading state, adjust sizes

--- src/components/site/newsletter.js
+++ src/components/site/newsletter.js
@@ -7,10 +7,12 @@ import { Link } from '../default/Link';
 
 export default function Newsletter({ widget = false }) {
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { siteMetadata } = useSiteMetadata();
 
   const handleSubmit = event => {
     event.preventDefault();
+    setLoading(true);
     const data = new FormData(event.target);
     fetch('https://paritytechnologies.activehosted.com/proc.php', {
       method: 'POST',
@@ -18,13 +20,17 @@ export default function Newsletter({ widget = false }) {
       mode: 'no-cors',
     })
       .then(response => {
+        setLoading(false);
         console.log(response);
         setFormSubmitted(!formSubmitted);
         setTimeout(() => {
           setFormSubmitted(false);
-        }, 5000);
+        }, 15000);
       })
-      .catch(error => console.log('Request failed', error));
+      .catch(error => {
+        setLoading(false);
+        console.log('Request failed', error);
+      });
   };
 
   return (
@@ -33,15 +39,22 @@ export default function Newsletter({ widget = false }) {
         <div
           className={cn('w-full border-b-2 border-gray-600', {
             ' pt-0 pb-0 border-b-0': widget,
-            'pt-12 pb-8': !widget,
+            'pt-8 pb-4': !widget,
           })}
         >
           <div className="flex items-center">
             {!widget && <Icon name="paperplane" className="mb-4 mr-4 fill-current text-white" />}
-            <h2 className="text-4xl font-bold mb-4">Newsletter</h2>
+            <h2
+              className={cn('text-4xl font-bold mb-4', {
+                'text-2xl': !widget,
+                'text-4xl': widget,
+              })}
+            >
+              Newsletter
+            </h2>
           </div>
           <div className={cn('', { 'lg:grid lg:grid-cols-3 gap-6': !widget })}>
-            <p className="font-bold text-lg max-w-lg lg:max-w-7xl">
+            <p className="font-semibold text-lg max-w-lg lg:max-w-7xl">
               Subscribe for the latest news, technical updates and helpful developer resources.
             </p>
             <form className="col-span-2" onSubmit={event => handleSubmit(event)}>
@@ -55,10 +68,11 @@ export default function Newsletter({ widget = false }) {
               <div className={cn('', { 'lg:flex gap-6': !widget })}>
                 <input
                   className={cn(
-                    'w-full max-w-lg flex-1 lg:max-w-lg h-16 mb-6 border-3 rounded-lg border-black text-bodyBg text-xl p-4 focus:outline-none hover:ring-2 focus:ring-2 text-center',
+                    'w-full flex-1 h-16 mb-6 border-3 rounded-lg border-black text-bodyBg text-xl p-4 focus:outline-none hover:ring-2 focus:ring-2 text-center',
                     {
-                      'bg-substrateBlackish border-white text-white': !widget,
-                      'dark:bg-substrateDarkest dark:border-substrateGray-dark text-black dark:text-white': widget,
+                      'bg-substrateBlackish border-white text-white max-w-sm': !widget,
+                      'dark:bg-substrateDarkest dark:border-substrateGray-dark text-black dark:text-white max-w-lg':
+                        widget,
                     }
                   )}
                   type="email"
@@ -66,14 +80,19 @@ export default function Newsletter({ widget = false }) {
                   placeholder={!widget ? 'Type your Email Address' : 'Email Address'}
                   required
                 />
-                <div className="max-w-lg lg:w-lg flex-1">
+                <div
+                  className={cn('lg:w-lg flex-1', {
+                    'max-w-sm': !widget,
+                    'max-w-lg': widget,
+                  })}
+                >
                   <button
                     className="w-full bg-substrateGreen hover:bg-white text-white hover:text-substrateGreen align-items mb-4 px-9 py-4 text-xl font-bold transition duration-200 focus:outline-none focus:ring-2 rounded-lg border-3 border-substrateGreen"
                     type="submit"
                     value="Submit"
                     onSubmit={event => handleSubmit(event)}
                   >
-                    Subscribe
+                    {loading ? 'Loading' : 'Subscribe'}
                   </button>
                   <p className={cn('text-sm', { ' lg:text-center': !widget })}>
                     To see how we use your information please see our{' '}
@@ -94,7 +113,7 @@ export default function Newsletter({ widget = false }) {
             <h2 className="text-4xl font-bold mb-4">Newsletter</h2>
           </div>
           <div className="flex">
-            {!widget && <Icon name="check-mark" className="h-6 w-6 mb-4 ml-1 mr-5 rounded-full" />}
+            {!widget && <Icon data-aos="fade" name="check-mark" className="h-6 w-6 mb-4 ml-1 mr-5 rounded-full" />}
 
             <p className="font-bold text-lg">
               To complete the subscription process, please click the link in the email we just sent you.
