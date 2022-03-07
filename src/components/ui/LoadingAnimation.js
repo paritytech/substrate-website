533775b9cf309d29ebce7a68ae659c1f2af1a63d
Imad Arain <arain.imad@gmail.com>
2022-03-06 23:01:13 -0700

    hubspot onSubmit component + animation

--- src/components/ui/LoadingAnimation.js
+++ src/components/ui/LoadingAnimation.js
@@ -13,7 +13,7 @@ export default function LoadingAnimation() {
           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
           <path
             className="opacity-75"
-            fill="#FF1864"
+            fill="#24CC85"
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           ></path>
         </svg>
