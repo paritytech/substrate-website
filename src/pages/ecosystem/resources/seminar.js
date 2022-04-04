e94ed77dfecad71f8c07afef3097b172d311c013
Jon Eirik Boska <joneirikboska@gmail.com>
2022-04-04 11:39:32 +0200

    Fix linting errors

--- src/pages/ecosystem/resources/seminar.js
+++ src/pages/ecosystem/resources/seminar.js
@@ -2,8 +2,8 @@ import { graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
-import seminars from '../../../../data/seminars.json';
 import Icon from '../../../components/default/Icon';
+import { Link } from '../../../components/default/Link';
 import Section from '../../../components/layout/Section';
 import Layout from '../../../components/site/Layout';
 import SEO from '../../../components/site/SEO';
@@ -29,7 +29,9 @@ export default function Seminar() {
               building on Substrate. Run by DevHub, we meet <span className="font-bold">every other Tuesday</span> at{' '}
               <span className="font-bold">14:00UTC.</span>
             </p>
-            <PrimaryButtonLink link="https://github.com/sacha-l/substrate-seminar/issues/new">Propose a topic</PrimaryButtonLink>
+            <PrimaryButtonLink link="https://github.com/sacha-l/substrate-seminar/issues/new">
+              Propose a topic
+            </PrimaryButtonLink>
             <p className="mb-4 mt-8 font-bold">Add the schedule to your calendar</p>
             <SecondaryButton link="https://calendar.google.com/calendar/u/0?cid=Y192cXBsamk3cXY2ajBvcDVrbmdwMGR0cjUzc0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t">
               <span className="inline font-bold mr-3">Add schedule</span>
@@ -54,23 +56,22 @@ export default function Seminar() {
               <span className="underline-animate underline-animate-thin">
                 <Link to="https://www.crowdcast.io/e/substrate-seminar-2/">on Crowdcast</Link>.
               </span>
-              , where attendees are encouraged to ask questions and interact directly with the presenters. 
-              They typically take the format of learning from other developers using Substrate, who 
-              present their insights on what they&apos;re working on.
+              , where attendees are encouraged to ask questions and interact directly with the presenters. They
+              typically take the format of learning from other developers using Substrate, who present their insights on
+              what they&apos;re working on.
             </p>
             <p className="leading-loose">
-              The end of a seminar is always open for Q & A, so bring your questions. Got code
-              that doesn’t compile? Bring that along too! Participants will be invited on-screen to share their work
-              and their questions if they want to. You may also want to provide feedback to an upcoming seminar plan by looking through
-              the proposed topics in the{' '}
+              The end of a seminar is always open for Q & A, so bring your questions. Got code that doesn’t compile?
+              Bring that along too! Participants will be invited on-screen to share their work and their questions if
+              they want to. You may also want to provide feedback to an upcoming seminar plan by looking through the
+              proposed topics in the{' '}
               <span className="underline-animate underline-animate-thin">
                 <Link to="https://github.com/sacha-l/substrate-seminar">dedicated GitHub repository</Link>.
               </span>
             </p>
             <p className="leading-loose">
-              Seminars are generally developer-oriented, but less technical participants and questions are also
-              welcome. If your questions turn out to be off-topic, we&apos;ll make sure to point you to the resources
-              you need.
+              Seminars are generally developer-oriented, but less technical participants and questions are also welcome.
+              If your questions turn out to be off-topic, we&apos;ll make sure to point you to the resources you need.
             </p>
           </div>
           <div className="order-first md:order-last">
@@ -78,9 +79,11 @@ export default function Seminar() {
             <p className="leading-loose">
               Check out our{' '}
               <span className="underline-animate underline-animate-thin">
-                <Link to="https://www.youtube.com/playlist?list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS">Youtube playlist</Link>
-              </span>
-              {' '}to revisit past seminars.
+                <Link to="https://www.youtube.com/playlist?list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS">
+                  Youtube playlist
+                </Link>
+              </span>{' '}
+              to revisit past seminars.
             </p>
           </div>
         </div>
