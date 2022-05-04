c0c3fe07fae9f0499b3420867a397abb7685d453
Jon Eirik Boska <joneirikboska@gmail.com>
2022-05-04 12:05:01 +0200

    Store YouTube data in json, use for rendering Past Seminars page

--- src/pages/ecosystem/resources/past-seminars.js
+++ src/pages/ecosystem/resources/past-seminars.js
@@ -1,16 +1,14 @@
-import { format } from 'date-fns';
 import { graphql } from 'gatsby';
 import React from 'react';
 
+import seminars from '../../../../data/seminars.json';
 import Icon from '../../../components/default/Icon';
 import Section from '../../../components/layout/Section';
 import Layout from '../../../components/site/Layout';
 import SEO from '../../../components/site/SEO';
 import LineArrowButton from '../../../components/ui/LineArrowButton';
 
-export default function PastSeminars({ data }) {
-  const { videos } = data.youtubePlaylist;
-
+export default function PastSeminars() {
   return (
     <Layout layout="sidebar">
       <SEO title="Past Seminars" />
@@ -21,14 +19,14 @@ export default function PastSeminars({ data }) {
       </Section>
       <Section>
         <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
-          {videos.map(({ id, title, publishedAt }, idx) => (
+          {seminars.map(({ id, title, date }, idx) => (
             <div key={idx} className="shadow-xl p-8 max-w-md rounded-md bg-white dark:bg-substrateBlackish">
               <h3 className="text-xl font-bold mb-4">
                 {title.slice(0, 18) === 'Substrate Seminar:' ? title.slice(18) : title}
               </h3>
               <div className="flex mb-4">
                 <Icon className="mr-2 mt-0.5 fill-current text-black dark:text-white" name="date" />
-                <p className="mb-0">{format(new Date(publishedAt), 'dd MMM yyyy')}</p>
+                <p className="mb-0">{date}</p>
               </div>
               <LineArrowButton link={`https://www.youtube.com/watch?v=${id}`} primary>
                 Watch
@@ -49,18 +47,6 @@ export const query = graphql`
           ns
           data
           language
-        }
-      }
-    }
-    youtubePlaylist {
-      videos {
-        id
-        title
-        publishedAt
-        cover {
-          childImageSharp {
-            gatsbyImageData(width: 200, layout: FIXED, placeholder: BLURRED)
-          }
         }
       }
     }
