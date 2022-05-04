24eb2310b67a5d9745a306b541d7ae3c32570de6
Jon Eirik Boska <joneirikboska@gmail.com>
2022-05-04 13:19:04 +0200

    Remove hero prop from SecondaryButton

--- src/components/ui/SecondaryButton.js
+++ src/components/ui/SecondaryButton.js
@@ -3,17 +3,13 @@ import React from 'react';
 
 import { Link } from '../default/Link';
 
-const SecondaryButton = ({ children, className, link, hero }) => {
+const SecondaryButton = ({ children, className, link }) => {
   return (
     <Link to={link}>
       <div
         className={cx(
           className +
-            ' bg-substrateBlackish dark:bg-substrateDarkThemeLightGrey inline-block rounded-md hover:opacity-80 transition-opacity',
-          {
-            'px-8 py-4 text-xl': hero,
-            'px-5 py-2': !hero,
-          }
+            ' bg-substrateBlackish dark:bg-substrateDarkThemeLightGrey inline-block rounded-md hover:opacity-80 transition-opacity px-5 py-2'
         )}
       >
         <div className="font-bold mb-0 text-white dark:text-black">{children}</div>
