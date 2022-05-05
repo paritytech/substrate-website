425eb24a3c8de4fa7ca9938d10963ff42eea3bce
Jon Eirik Boska <joneirikboska@gmail.com>
2022-05-05 16:21:45 +0200

    Create ExploreCommunityProjects component with data

--- src/pages/contracts.js
+++ src/pages/contracts.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Icon from '../components/default/Icon';
 import { Link } from '../components/default/Link';
 import Card from '../components/layout/Card';
+import ExploreCommunityProjects from '../components/layout/contracts/ExploreCommunityProjects';
 import Section from '../components/layout/Section';
 import Layout from '../components/site/Layout';
 import SEO from '../components/site/SEO';
@@ -64,49 +65,58 @@ export default function SmartContracts() {
         <div>Illustration</div>
       </Section>
 
-      <Section className="grid sm:grid-cols-2 gap-8 max-w-5xl justify-center my-36">
-        {tutorials.map(({ title, description, image, difficulty, time }, idx) => (
-          <div
-            key={idx}
-            className="bg-substrateGray-light dark:bg-substrateBlackish rounded-md max-w-md overflow-hidden"
-          >
-            <div className="overflow-hidden">
-              <img
-                src={image}
-                alt={title}
-                className="overflow-hidden w-full h-24 md:h-60 object-cover hover:scale-110 transition-transform"
-              />
-            </div>
-            <div className="p-4">
-              <h4 className="text-2xl mb-6">{title}</h4>
-              <p className="text-base mb-6">{description}</p>
-              <div className="flex justify-between items-center">
-                <div className="flex">
-                  <p className="mb-0 text-sm uppercase tracking-wider">
-                    {difficulty === 1 ? 'Beginner' : difficulty === 2 ? 'Intermediate' : difficulty === 3 && 'Advanced'}
-                  </p>
-                  <div className="flex ml-1 mt-1">
-                    {[...Array(3)].map((_, idx) => (
-                      <span
-                        key={idx}
-                        className={cx('mx-1 inline-block w-3 h-3 bg-substrateDark dark:bg-substrateGray rounded-full', {
-                          'bg-opacity-50': difficulty <= idx,
-                        })}
-                      ></span>
-                    ))}
+      <Section>
+        <div className="grid sm:grid-cols-2 gap-8 justify-center my-36 max-w-5xl">
+          {tutorials.map(({ title, description, image, difficulty, time }, idx) => (
+            <div
+              key={idx}
+              className="bg-substrateGray-light dark:bg-substrateBlackish rounded-md max-w-md overflow-hidden"
+            >
+              <div className="overflow-hidden">
+                <img
+                  src={image}
+                  alt={title}
+                  className="overflow-hidden w-full h-24 md:h-60 object-cover hover:scale-110 transition-transform"
+                />
+              </div>
+              <div className="p-4">
+                <h4 className="text-2xl mb-6">{title}</h4>
+                <p className="text-base mb-6">{description}</p>
+                <div className="flex justify-between items-center">
+                  <div className="flex">
+                    <p className="mb-0 text-sm uppercase tracking-wider">
+                      {difficulty === 1
+                        ? 'Beginner'
+                        : difficulty === 2
+                        ? 'Intermediate'
+                        : difficulty === 3 && 'Advanced'}
+                    </p>
+                    <div className="flex ml-1 mt-1">
+                      {[...Array(3)].map((_, idx) => (
+                        <span
+                          key={idx}
+                          className={cx(
+                            'mx-1 inline-block w-3 h-3 bg-substrateDark dark:bg-substrateGray rounded-full',
+                            {
+                              'bg-opacity-10 dark:bg-opacity-20': difficulty <= idx,
+                            }
+                          )}
+                        ></span>
+                      ))}
+                    </div>
                   </div>
-                </div>
-                <div className="flex items-center">
-                  <Icon name="time" />
-                  <span>{time}</span>
+                  <div className="flex items-center">
+                    <Icon name="time" />
+                    <span>{time}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </Section>
 
-      <Section>
+      <Section className="mb-36">
         <h2 className="text-4xl font-extrabold mb-8">Explore Code Samples</h2>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center auto-rows-fr">
           {cards.map(({ title, description, link }, idx) => (
@@ -126,6 +136,7 @@ export default function SmartContracts() {
       <Section>
         <h2 className="text-4xl font-extrabold mb-8">Explore Community Projects</h2>
         <p className="text-base mb-6">Browse through different networks, tooling and further resources.</p>
+        <ExploreCommunityProjects />
       </Section>
     </Layout>
   );
