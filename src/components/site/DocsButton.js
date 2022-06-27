3ba20496ff4d2b15359650477778081764260931
rmnprkrl <rmnprkrl@gmail.com>
2022-06-27 17:15:52 +0200

    docsbutton source webconsts

--- src/components/site/DocsButton.js
+++ src/components/site/DocsButton.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-import { useSiteMetadata } from '../../hooks/use-site-metadata';
+import { DOCS_URL } from '../../../config/webConsts';
 import Icon from '../default/Icon';
 import { Link } from '../default/Link';
 
 export default function DocsButton() {
-  const { siteMetadata } = useSiteMetadata();
-  const docsLink = siteMetadata.docsUrl;
-
   return (
     // TODO: add docs link
     // TODO: add docs title i18n
-    <Link to={docsLink} className="block opacity-100 transform transition duration-300 ease-in-out hover:opacity-80">
+    <Link to={DOCS_URL} className="block opacity-100 transform transition duration-300 ease-in-out hover:opacity-80">
       <button className="flex items-center justify-center bg-substrateDark dark:bg-white  text-sm py-2 w-20 rounded focus:outline-none fill-current text-white dark:text-black">
         <Icon name="docs" />
         <span className="pl-2">Docs</span>
