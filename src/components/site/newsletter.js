e0d4dffbd738d17b62a224779547e08bbdcf254d
rcos7 <radu.costica@parity.io>
2022-07-14 09:47:09 +0100

    add extra margin

--- src/components/site/newsletter.js
+++ src/components/site/newsletter.js
@@ -79,7 +79,7 @@ export default function Newsletter({ layout = 'default' }) {
             />
             {isFormReady && (
               <p
-                className={cn('text-sm mt-4 lg:ml-auto', {
+                className={cn('text-sm mt-6 lg:mt-4 lg:ml-auto', {
                   'mb-0 max-w-sm lg:text-center': !widget,
                   'mb-6': widget,
                 })}
