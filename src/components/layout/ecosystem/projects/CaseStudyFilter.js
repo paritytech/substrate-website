736eff62636ea89bf31d626f5e132dce487221c7
jonnysmillie <jonnysmillie@jonnysmillie.co.uk>
2022-10-18 10:28:41 +0100

    update case study button, remove case study link on homepage

--- src/components/layout/ecosystem/projects/CaseStudyFilter.js
+++ src/components/layout/ecosystem/projects/CaseStudyFilter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CaseStudyFilter = ({ showCaseStudies, setShowCaseStudies }) => {
   return (
-    <div className="flex case-studies dark:bg-darkBackground border py-2 px-3 lg:justify-center justify-start items-center hover:cursor-pointer lg:mb-0 md:mb-4 sm:mb-4 xs:mb-4">
+    <div className="flex case-studies dark:bg-darkBackground lg:border py-2 px-3 lg:justify-center justify-start items-center hover:cursor-pointer lg:mb-0 md:mb-4 sm:mb-4 xs:mb-4">
       <p className="inline pr-3 cursor-pointer mb-0 text-sm" onClick={() => setShowCaseStudies(!showCaseStudies)}>
         Case Studies only
       </p>
