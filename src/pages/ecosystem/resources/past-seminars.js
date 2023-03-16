f2c61642dbf78b788147ce5acb1e4d3443a8c7ff
carinaluise <carinalrobinson@gmail.com>
2023-03-16 12:40:07 +0100

    add div and flex layout to live seminars 23 section

--- src/pages/ecosystem/resources/past-seminars.js
+++ src/pages/ecosystem/resources/past-seminars.js
@@ -21,14 +21,16 @@ export default function PastSeminars() {
       </Section>
       <Section>
         <h2 className="text-2xl font-normal font-body mb-6 ">Substrate Seminar Live Streams (2023)</h2>
-        <iframe
-          className="w-full lg:w-5/12 seminar-video rounded"
-          src="https://www.youtube-nocookie.com/embed/videoseries?list=PLOyWqupZ-WGsfgxkwTdMOwnbRW4nx_T-i"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <div className="flex flex-wrap gap-8">
+          <iframe
+            className="w-full lg:w-5/12 seminar-video rounded"
+            src="https://www.youtube-nocookie.com/embed/videoseries?list=PLOyWqupZ-WGsfgxkwTdMOwnbRW4nx_T-i"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </Section>
       <Section>
         <h2 className="text-2xl font-normal font-body mb-6">Substrate Seminar Live Streams (2021-2022)</h2>
