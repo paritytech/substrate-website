2234266c5bb205ea8d69f7f675ecb1c592c8bb16
carinaluise <carinalrobinson@gmail.com>
2023-11-17 14:13:55 +0100

    remove direct yt playlists, add cards

--- src/pages/ecosystem/resources/past-seminars.js
+++ src/pages/ecosystem/resources/past-seminars.js
@@ -1,53 +1,40 @@
 import { graphql } from 'gatsby';
-import { Layout, Section, SEO } from 'gatsby-plugin-substrate';
+import { Icon, Layout, LineArrowButton, Link, Section, SEO } from 'gatsby-plugin-substrate';
 import React from 'react';
 
-const oldPlaylists = [
-  'PLp0_ueXY_enU7jbm_A-3BrXiMbHPR0he0',
-  'PLp0_ueXY_enWgF4FEhMUOZgK7DQJIswDM',
-  'PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS',
-  'PLp0_ueXY_enVAh4AfpAJtdBNCxL_eCs9K',
+const Playlists = [
+  { id: 'PLOyWqupZ-WGsfgxkwTdMOwnbRW4nx_T-i', title: 'Substrate Seminars', date: 'Jul 2023' },
+  { id: 'PLp0_ueXY_enU7jbm_A-3BrXiMbHPR0he0', title: 'Runtime Development', date: 'Sept 2022' },
+  { id: 'PLp0_ueXY_enWgF4FEhMUOZgK7DQJIswDM', title: 'Application Development', date: 'Oct 2022' },
+  { id: 'PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS', title: 'Testing, Launching and Tooling', date: 'Oct 2022' },
+  { id: 'PLp0_ueXY_enVAh4AfpAJtdBNCxL_eCs9K', title: 'Parachain Builders', date: 'Sept 2022' },
 ];
 
 export default function PastSeminars() {
   return (
     <Layout layout="sidebar">
-      <SEO
-        title="Blockchain Past Webinars &amp; Seminars"
-        description="Blockchain Past Webinars &amp; Seminars. Access to all our content. Substrate blockchain enables developers to quickly and easily build future-proof blockchains optimized for any use case. "
-      />
+      <SEO title="Past Seminars" />
       <Section>
-        <h1 className="text-4xl font-bold font-body mb-6">Past Seminars</h1>
-      </Section>
-      <Section>
-        <h2 className="text-2xl font-normal font-body mb-6 ">Substrate Seminar Live Streams (2023)</h2>
-        <div className="flex flex-wrap gap-8">
-          <iframe
-            className="w-full lg:w-5/12 seminar-video rounded shadow-xl hover:scale-105 transition-transform"
-            src="https://www.youtube-nocookie.com/embed/videoseries?list=PLOyWqupZ-WGsfgxkwTdMOwnbRW4nx_T-i"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </Section>
-      <Section>
-        <h2 className="text-2xl font-normal font-body mb-6">Substrate Seminar Live Streams (2021-2022)</h2>
-        <div className="flex flex-wrap gap-8">
-          {oldPlaylists.map(playlist => {
-            return (
-              <iframe
-                key={playlist}
-                className="w-full lg:w-5/12 seminar-video rounded shadow-xl hover:scale-105 transition-transform"
-                src={`https://www.youtube-nocookie.com/embed/videoseries?list=${playlist}`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
-            );
-          })}
+        <h1 id="awesome-substrate" className="text-4xl font-bold font-body mb-6">
+          Past Seminars
+        </h1>
+        <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-6 auto-rows-fr">
+          {Playlists.map(({ id, title, date }, idx) => (
+            <Link to={`https://www.youtube.com/playlist?list=${id}`} key={idx}>
+              <div className="mb-1 h-full shadow-xl p-8 rounded-md bg-white dark:bg-substrateBlackish flex flex-col justify-between hover:scale-105 transition-transform">
+                <h3 className="mb-4 text-xl font-bold">{title}</h3>
+                <div>
+                  <div className="flex mb-2">
+                    <Icon className="mr-2 mt-0.5 fill-current text-black dark:text-white" name="date" />
+                    <p className="mb-0">{date}</p>
+                  </div>
+                  <LineArrowButton link={`https://www.youtube.com/playlist?list=${id}`} primary>
+                    Watch Playlist
+                  </LineArrowButton>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </Section>
     </Layout>
